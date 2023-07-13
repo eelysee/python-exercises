d0ca634b4d8465f8d83fe510ea8384c3345cb0e8
Edwige Elysee <edwige.elysee@gmail.com>
2023-07-13 16:42:16 -0500

    finished working checkbook app

--- python_checkbook_project/checkbook.py
+++ python_checkbook_project/checkbook.py
@@ -9,14 +9,15 @@ import os
 # b_d = bank_data.json
 
 # import from json read files
+# balance is an int
 with open ('bank_data.json', 'r') as b_d:
     balance = json.load(b_d)
 # print(balance)
 
-# write to files
-with open ('bank_data.json', 'w') as b_d:
-    json.dump(balance, b_d)
- 
+
+# defined balnce as as int
+# print(type(balance))
+
 # Landing Page
 print()
 print()
@@ -28,43 +29,49 @@ print(' 3) record a credit (deposit)')
 print(' 4) exit')
 print()
 
-def home_screen():
-    while True:
-        fi = input('what would you like to do?')
-        print('~~~ Welcome to your terminal checkbook! ~~~')
-        print(' What would you like to do?')
-        print(' 1) view current balance')
-        print(' 2) record a debit (withdraw)')
-        print(' 3) record a credit (deposit)')
-        print(' 4) exit')
+while True:
+    fi = input('what would you like to do?')
+    print('~~~ Welcome to your terminal checkbook! ~~~')
+    print(' What would you like to do?')
+    print(' 1) view current balance')
+    print(' 2) record a debit (withdraw)')
+    print(' 3) record a credit (deposit)')
+    print(' 4) exit')
+
+    if fi == '4':
+        print()
+        print()
+        print('Good bye!')
+        with open ('bank_data.json', 'w') as b_d:
+            json.dump(balance, b_d)
+        break
+
+    elif fi == '1':
+        print()
+        print()
+        print('This is your current balance:')
+        print()
+        print(balance)
+        print()
+
+    elif fi == '2':
+        print()
+        print()
+        d = int(input('Please enter amout to debit from your account!'))
+        balance -= d
+
+    elif fi == '3':
+        c = int(input('Please enter amount to credit to your account!'))
+        balance += c
         
-        if fi == '4':
-            print()
-            print()
-            print('Good bye!')
-            break
-        elif fi == '1':
-            print()
-            print()
-            print('This is your current balance:')
-            print()
-            print(balance)
-            print()
-           # b_d
-        elif fi == '2':
-            print()
-            print()
-            input('Please enter amout to debit from your account!')
-    
-        elif fi == '3':
-            print('Please enter amount to credit to your account!')
-            #with open (simple_bank_data.txt, 'r') as b_d:
-            #b_d.write(input())
-        else:
-            print()
-            print()
-            print('PLEASE ENTER A VALID RESPONSE.')
-            continue
+    else:
+        print()
+        print()
+        print('PLEASE ENTER A VALID RESPONSE.')
+        continue
 
 
-home_screen()
+# write to files
+#with open ('bank_data.json', 'w') as b_d:
+#    json.dump(balance, b_d)
+ 
